acbe3d342f8fb2a2462da61007b67a24b678fd07
Is2dann <b.daniel.oliver@gmail.com>
2025-09-07 23:40:54 +0100

    Added admin navbar link for staff only. Admin restrictions. and email tetst

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,8 +1,11 @@
 from unittest.mock import patch
 from decimal import Decimal
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.core import mail
 from django.urls import reverse
 from products.models import Product
+from checkout.models import Order, OrderLineItem
+from checkout.emails import send_order_confirmation
 
 
 class StripeFlowTests(TestCase):
@@ -29,3 +32,21 @@ class StripeFlowTests(TestCase):
         s.save()
         r = self.client.get(reverse('checkout:checkout_paid') + '?payment_intent_client_secret=pi_123_secret_abc')
         self.assertEqual(r.status_code, 302)
+
+
+@override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
+class EmailTests(TestCase):
+    def test_order_mail(self):
+        p = Product.objects.create(name='X', price=Decimal('10.00'))
+        o = Order.objects.create(
+            full_name='T', email='t@t.com', phone_number='1',
+            address1='A', city='C', postcode='P', country='GB',
+            order_total=Decimal('10.00'), delivery_cost=Decimal('10.00'),
+            grand_total=Decimal('10.00')
+        )
+        OrderLineItem.objects.create(
+            order=o, product=p, quantity=1
+        )
+        send_order_confirmation(o)
+        self.assertTrue(mail.outbox)
+        self.assertIn(str(o.order_number), mail.outbox[0].subject)
