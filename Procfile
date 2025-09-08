0c6303a44f4e69e4ba78bd341182400d20aaedc7
Is2dann <b.daniel.oliver@gmail.com>
2025-09-08 22:01:25 +0100

    Prepared database and app for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cwh_site.wsgi:application
--- cwh_site/settings.py
+++ cwh_site/settings.py
@@ -41,7 +41,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = [
     '127.0.0.1',  # VSCode preview
     'localhost',  # listen for stripe webhooks
-    '',  # Heroku app will come here after deployment
+    'creations-w-happycilline-54784e178ac3.herokuapp.com',  # Heroku
 ]
 
 
@@ -145,12 +145,17 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
